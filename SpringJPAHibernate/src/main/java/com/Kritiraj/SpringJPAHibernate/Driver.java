22c25e93abac8c35f0060bcbd514ce93cbbbd8c7
KritirajChakraborty <kritiraj.exams2021@gmail.com>
2026-01-09 21:49:07 +0530

    One-one relationship and unidirectional mapping

--- SpringJPAHibernate/src/main/java/com/Kritiraj/SpringJPAHibernate/Driver.java
+++ SpringJPAHibernate/src/main/java/com/Kritiraj/SpringJPAHibernate/Driver.java
@@ -25,4 +25,11 @@ public class Driver {
     @OneToMany(cascade= CascadeType.ALL)
     @JoinColumn(name="driver_id")
     List<Booking> bookings = new ArrayList<>();
+
+    @OneToOne(cascade=CascadeType.ALL)  //in 1-1 relationships, FK is added on the table where JoinColumn(Driver Entity) is called
+    //unlike 1-many where FK is always created on the many table
+    //here Driver and Cab can have multi directional mapping since it is one-one but this is avoided in real
+    //life production code. Always prefer unidirectional mapping
+    @JoinColumn(name="cab_id")
+    Cab cab;
 }
